56956b8bcab55133792d5910052567255f36af65
suman jaiswal <jsonusuman351@gmail.com>
2025-09-06 13:33:13 +0530

    commit structuredoutputparser

---
+++ structuredoutputparser.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-R1", task="text-generation")
+
+model=ChatHuggingFace(llm=llm)
+
+schema = [
+    ResponseSchema(name='fact_1', description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2', description='Fact 2 about the topic'),
+    ResponseSchema(name='fact_3', description='Fact 3 about the topic'),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template='Give 3 facts about {topic}. Return your answer as a JSON object with the following keys: fact_1, fact_2, fact_3.\nOnly output the JSON object, do not include any explanation or extra text.\n{format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
